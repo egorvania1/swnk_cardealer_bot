d1666483030ee08f846b2bfd652859b8aa8de79e
egorvania1 <egorvania1@gmail.com>
2025-01-17 22:57:24 +0500

    Показ инфы из БД через бота
    Функции в show_tables теперь возвращают данные через return, а не print.
    Подключение к БД через функции в dbmanage

--- bot.py
+++ bot.py
@@ -1,10 +1,20 @@
 import os
 from dotenv import load_dotenv
 
-import logging
-from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+import psycopg2
 
+from show_tables import show_workers, show_cars, show_shops, show_orders
+
+import logging
+from telegram import ForceReply, Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    ConversationHandler,
+    MessageHandler,
+    filters,
+)
 load_dotenv()
 
 # Enable logging
@@ -16,6 +26,7 @@ logging.basicConfig(
 logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
+WORKERS, BUYERS, DEALERS, CARS, SHOPS, ORDERS, CHOICE = range(7)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
@@ -27,70 +38,69 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /help is issued."""
-    await update.message.reply_text("Help!")
-
+    await update.message.reply_text("/start, /view, /insert")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     await update.message.reply_text(update.message.text)
 
+async def view(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    reply_keyboard = [["Сотрудники", "Автомобили", "Автосалоны", "Заказы"]]
+    await update.message.reply_text(
+        "Что вы хотите просмотреть?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Выбор"
+        ),
+    )
+    return CHOICE
+    
+async def select_view(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    logger.info("Selected view: %s", update.message.text)
+    result = "Ошибка"
+    match update.message.text:
+        case "Сотрудники":
+            result = show_workers()
+        case "Автомобили":
+            result = show_cars()
+        case "Автосалоны":
+            result = show_shops()
+        case "Заказы":
+            result = show_orders()
+    #result = [(x.encode('utf-8') for x in tup) for tup in result]
+    logger.info("Printing to %s: %s", update.message.from_user, result)
+    await update.message.reply_text(result)
+    return ConversationHandler.END
 
-async def shops(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-async def workers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-async def jobs(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-async def customers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-async def price(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-async def cars(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
-
-async def dealers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Отмена.", reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
 
 def main() -> None:
     """Start the bot."""
     # Create the Application and pass it your bot's token.
     application = Application.builder().token(os.getenv("API_KEY")).build()
-
-    # Help commands
-    application.add_handler(CommandHandler("start", start))
+    
+    view_handler = ConversationHandler(
+        entry_points=[CommandHandler("view", view)],
+        states={
+            CHOICE: [MessageHandler(filters.Regex("^(Сотрудники|Автомобили|Автосалоны|Заказы)$"), select_view)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+    
+    # Conversation handlers
+    application.add_handler(view_handler)
+    
+    # Help command
     application.add_handler(CommandHandler("help", help_command))
     
-    # Function commands
-    application.add_handler(CommandHandler("shops", shops))
-    application.add_handler(CommandHandler("workers", workers))
-    application.add_handler(CommandHandler("jobs", jobs))
-    application.add_handler(CommandHandler("customers", customers))
-    application.add_handler(CommandHandler("price", price))
-    application.add_handler(CommandHandler("cars", cars))
-    application.add_handler(CommandHandler("dealers", dealers))
-
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
--- show_tables.py
+++ show_tables.py
@@ -1,25 +1,38 @@
 import psycopg2
+import psycopg2.extensions
 
-def show_workers(conn):
+from dbmanage import connectdb
+
+def show_workers():
+    conn = connectdb()
     with conn.cursor() as cur:
         cur.execute('''SELECT workers.workerId, workers.FIO, workers.skips, workers.position, workers.experience, positions.pay
         FROM workers
         INNER JOIN positions
         ON workers.position = positions.position AND workers.experience = positions.experience
         ORDER BY workers.workerId;''')
-        print(cur.fetchall())
+        result = cur.fetchall()
+    conn.close()
+    return result
         
-def show_cars(conn):
+def show_cars():
+    conn = connectdb()
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM cars ORDER BY cars.vin;")
-        print(cur.fetchall())
+        result = cur.fetchall()
+    conn.close()
+    return result
         
-def show_shops(conn):
+def show_shops():
+    conn = connectdb()
     with conn.cursor() as cur:
         cur.execute("SELECT * FROM shops ORDER BY shops.shopId;")
-        print(cur.fetchall())
+        result = cur.fetchall()
+    conn.close()
+    return result
         
-def show_orders(conn):
+def show_orders():
+    conn = connectdb()
     with conn.cursor() as cur:
         cur.execute('''SELECT s.name, s.address, w.FIO, c.brand, c.name, b.FIO, tp.totalPrice
         FROM keys_table AS kt
@@ -30,4 +43,6 @@ def show_orders(conn):
         INNER JOIN buyers b ON kt.buyerId = b.buyerId AND tp.buyerId = b.buyerId
         INNER JOIN dealers d ON kt.dealerId = d.dealerId AND tp.dealerId = d.dealerId
         ;''')
-        print(cur.fetchall())
+        result = cur.fetchall()
+    conn.close()
+    return result
